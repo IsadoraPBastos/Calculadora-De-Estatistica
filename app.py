77a68e15f03a70241850f6d89cf68618a76641c3
Isadora Bastos <isabas2803@gmail.com>
2025-08-31 21:26:55 -0300

    Programa Calculando a Media

--- app.py
+++ app.py
@@ -15,10 +15,10 @@ def dados_desordenados():
     if request.method == "POST":
         if request.form.get("dado"):
             dadosDesordenados.append(float(request.form.get("dado")))
-    return render_template("index.html", dadosDesordenados=dadosDesordenados, FequenciaIndividualAbsoluta={},FrequenciaAcumulada={}, Posicoes={})
+    return render_template("index.html", dadosDesordenados=dadosDesordenados, FequenciaIndividualAbsoluta={},FrequenciaAcumulada={}, Posicoes={}, FequenciaIndividualAbsolutaRecebida = {})
 
-FequenciaIndividualAbsoluta = {}
 
+FequenciaIndividualAbsolutaRecebida = {}
 #Recebe os dados quando a pessoa envia eles pela parte de tabela
 @app.route("/dados_em_tabela", methods=["POST", "GET"])
 def dados_em_tabela():
@@ -28,9 +28,9 @@ def dados_em_tabela():
             amostra = float(request.form.get('amostra'))
             frequencia = float(request.form.get('frequencia'))
 
-            FequenciaIndividualAbsoluta[amostra] = frequencia
+            FequenciaIndividualAbsolutaRecebida[amostra] = frequencia
             
-    return render_template("index.html", FequenciaIndividualAbsoluta=FequenciaIndividualAbsoluta,FrequenciaAcumulada={}, Posicoes={})
+    return render_template("index.html", FequenciaIndividualAbsolutaRecebida = FequenciaIndividualAbsolutaRecebida, FequenciaIndividualAbsoluta={},FrequenciaAcumulada={}, Posicoes={})
 
 #Limpa os dados inseridos
 @app.route("/limpar_dados", methods=["POST"])
@@ -39,17 +39,30 @@ def limpar_dados():
     dadosDesordenados = []
     global FequenciaIndividualAbsoluta
     FequenciaIndividualAbsoluta = {}
-    return render_template("index.html", FequenciaIndividualAbsoluta={},FrequenciaAcumulada={}, Posicoes={})
+    global FequenciaIndividualAbsolutaRecebida
+    FequenciaIndividualAbsolutaRecebida = {}
+    return render_template("index.html", FequenciaIndividualAbsoluta={},FrequenciaAcumulada={}, Posicoes={}, FequenciaIndividualAbsolutaRecebida = {})
+
+FequenciaIndividualAbsoluta = {}
 
 #Realiza as contas
 @app.route("/calculo_dos_dados", methods=["POST"])
 def calculo_dos_dados():
+    global FequenciaIndividualAbsoluta
+    global FequenciaIndividualAbsolutaRecebida
+    if FequenciaIndividualAbsolutaRecebida:
+        FequenciaIndividualAbsoluta = dict(sorted(FequenciaIndividualAbsolutaRecebida.items()))
     #Cálculo do FI e organização da tabela se os dados inseridos forem os desordenados
-    for dado in sorted(dadosDesordenados):
-        if dado in FequenciaIndividualAbsoluta:
-            FequenciaIndividualAbsoluta[dado] += 1
-        else:
-            FequenciaIndividualAbsoluta[dado] = 1
+    else :
+        for dado in sorted(dadosDesordenados):
+            if dado in FequenciaIndividualAbsoluta:
+                FequenciaIndividualAbsoluta[dado] += 1
+            else:
+                FequenciaIndividualAbsoluta[dado] = 1
+                
+                
+                
+    
             
     #Tamanho da amostra
     tamanhoDaAmostra = f"{sum(FequenciaIndividualAbsoluta.values()):g}"
@@ -61,17 +74,27 @@ def calculo_dos_dados():
     Posicoes = {}
     for dado in sorted(FequenciaIndividualAbsoluta.keys()):
         Fac = FacAnt + FequenciaIndividualAbsoluta[dado]
-        FrequenciaAcumulada[dado] = Fac
+        FacAjust = f"{Fac:g}"
+        FrequenciaAcumulada[dado] = FacAjust
         
         #Calculo de posições 
         inicio = FacAnt + 1
         fim = Fac
+        
+        inicio = f"{inicio:g}"
+        fim = f"{fim:g}" 
         Posicoes[dado] = f"{inicio}º à {fim}º" 
         
         FacAnt = Fac       
+    xifi = {}
     
+    #Média
+    for amostra, frequencia in sorted(FequenciaIndividualAbsoluta.items()):
+        xifi[amostra] = amostra * frequencia
         
-    return render_template("index.html", dadosDesordenados=dadosDesordenados, FequenciaIndividualAbsoluta=FequenciaIndividualAbsoluta, tamanhoDaAmostra=tamanhoDaAmostra, FrequenciaAcumulada=FrequenciaAcumulada, Posicoes=Posicoes)
+    media = round(sum(xifi.values())/sum(FequenciaIndividualAbsoluta.values()), 2)
+          
+    return render_template("index.html", media=media, dadosDesordenados=dadosDesordenados, FequenciaIndividualAbsoluta=FequenciaIndividualAbsoluta, tamanhoDaAmostra=tamanhoDaAmostra, FrequenciaAcumulada=FrequenciaAcumulada, Posicoes=Posicoes, FequenciaIndividualAbsolutaRecebida = {})
 
 if __name__ == '__main__':
     app.run(debug=True)
