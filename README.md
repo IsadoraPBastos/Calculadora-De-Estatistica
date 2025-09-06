23e62c503ec2528ee6c81ee014b3029271526307
Isadora Bastos <isabas2803@gmail.com>
2025-09-06 14:00:05 -0300

    Inclusão escolha dos cálculos

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Calculadora-De-Estatistica
-Calculadora para as aulas de estatistica 
+
+Calculadora para as aulas de estatistica
+
+Consertar -
+Botão todos no /calculo_dos_dados
+Botão de fechar modal tem q ser X e no canto direito
+Descer botões quando abrir o modal
--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 # pip install flask
+import json
 from flask import Flask, render_template, request
 from collections import Counter
 
@@ -8,7 +9,8 @@ dadosDesordenados = []
 
 @app.route('/')
 def index():
-    return render_template('index.html', FequenciaIndividualAbsoluta={},FrequenciaAcumulada={}, Posicoes={}, FequenciaIndividualAbsolutaRecebida = {})
+    return render_template('index.html', FequenciaIndividualAbsoluta={},FrequenciaAcumulada={}, Posicoes={}, 
+                           FequenciaIndividualAbsolutaRecebida = {}, escolhaCalculo=[])
 
 #Recebe os dados quando a pessoa envia eles pela parte desordenada 
 @app.route("/dados_desordenados", methods=["POST", "GET"])
@@ -16,9 +18,8 @@ def dados_desordenados():
     if request.method == "POST":
         if request.form.get("dado"):
             dadosDesordenados.append(float(request.form.get("dado")))
-            print("mostrar_modal:", "desordenado")
-            print("dadosDesordenados:", dadosDesordenados)
-    return render_template("index.html", mostrar_modal="desordenado", dadosDesordenados=dadosDesordenados, FequenciaIndividualAbsoluta={},FrequenciaAcumulada={}, Posicoes={}, FequenciaIndividualAbsolutaRecebida = {})
+    return render_template("index.html", mostrar_modal="desordenado", dadosDesordenados=dadosDesordenados, 
+    FequenciaIndividualAbsoluta={},FrequenciaAcumulada={}, Posicoes={}, FequenciaIndividualAbsolutaRecebida = {}, escolhaCalculo=[])
 
 
 FequenciaIndividualAbsolutaRecebida = {}
@@ -33,7 +34,9 @@ def dados_em_tabela():
 
             FequenciaIndividualAbsolutaRecebida[amostra] = frequencia
             
-    return render_template("index.html", mostrar_modal="tabela", FequenciaIndividualAbsolutaRecebida = FequenciaIndividualAbsolutaRecebida, FequenciaIndividualAbsoluta={},FrequenciaAcumulada={}, Posicoes={})
+    return render_template("index.html", mostrar_modal="tabela", 
+    FequenciaIndividualAbsolutaRecebida=FequenciaIndividualAbsolutaRecebida, FequenciaIndividualAbsoluta={},
+    FrequenciaAcumulada={}, Posicoes={}, escolhaCalculo=[])
 
 #Limpa os dados inseridos
 @app.route("/limpar_dados", methods=["POST"])
@@ -44,110 +47,126 @@ def limpar_dados():
     FequenciaIndividualAbsoluta = {}
     global FequenciaIndividualAbsolutaRecebida
     FequenciaIndividualAbsolutaRecebida = {}
-    return render_template("index.html", FequenciaIndividualAbsoluta={},FrequenciaAcumulada={}, Posicoes={}, FequenciaIndividualAbsolutaRecebida = {})
+    return render_template("index.html", FequenciaIndividualAbsoluta={},FrequenciaAcumulada={}, Posicoes={}, 
+    FequenciaIndividualAbsolutaRecebida = {}, escolhaCalculo=[])
 
 FequenciaIndividualAbsoluta = {}
 
 #Realiza as contas
 @app.route("/calculo_dos_dados", methods=["POST"])
 def calculo_dos_dados():
-    tipo = request.form.get("tipo")
-
-    if tipo == "outro":
-        tipo = request.form.get("tipo_custom")
     global FequenciaIndividualAbsoluta
     global FequenciaIndividualAbsolutaRecebida
-
-    modal_aberto = request.form.get("modal_aberto", "")
-
-    if FequenciaIndividualAbsolutaRecebida:
-        FequenciaIndividualAbsoluta = dict(sorted(FequenciaIndividualAbsolutaRecebida.items()))
-    #Cálculo do FI e organização da tabela se os dados inseridos forem os desordenados
-    else :
-        for dado in sorted(dadosDesordenados):
-            if dado in FequenciaIndividualAbsoluta:
-                FequenciaIndividualAbsoluta[dado] += 1
-            else:
-                FequenciaIndividualAbsoluta[dado] = 1
-                
-                
-                
     
+    
+    tipo = request.form.get("tipo")
+    if tipo == "outro":
+        tipo = request.form.get("tipo_custom")
             
-    #Tamanho da amostra
-    tamanhoDaAmostra = f"{sum(FequenciaIndividualAbsoluta.values()):g}"
-    
-    #Frequência acumulada absoluta
-    Fac = 0 
-    FacAnt = 0
-    FrequenciaAcumulada = {}
-    Posicoes = {}
-    for dado in sorted(FequenciaIndividualAbsoluta.keys()):
-        Fac = FacAnt + FequenciaIndividualAbsoluta[dado]
-        FacAjust = f"{Fac:g}"
-        FrequenciaAcumulada[dado] = FacAjust
+    escolhaCalculo = request.form.getlist("escolha-calculo")
+    escolhaCalculoJson = json.dumps(escolhaCalculo)
         
-        #Calculo de posições 
-        inicio = FacAnt + 1
-        fim = Fac
+    modal_aberto = request.form.get("modal_aberto", "")
+    try:
+        #Mensagem de erro caso a pessoa não insira um valor para os cálculos 
+        if not FequenciaIndividualAbsolutaRecebida and not dadosDesordenados:
+            raise ValueError
         
-        inicio = f"{inicio:g}"
-        fim = f"{fim:g}" 
-        if(inicio == fim):
-            Posicoes[dado] = f"{fim}º"
+        #Organização dos dados inseridos em tabela
+        elif FequenciaIndividualAbsolutaRecebida:
+            FequenciaIndividualAbsoluta = dict(sorted(FequenciaIndividualAbsolutaRecebida.items()))
+            
+        #Cálculo do FI e organização da tabela se os dados inseridos forem os desordenados
+        else :
+            for dado in sorted(dadosDesordenados):
+                if dado in FequenciaIndividualAbsoluta:
+                    FequenciaIndividualAbsoluta[dado] += 1
+                else:
+                    FequenciaIndividualAbsoluta[dado] = 1
+                    
+                    
+                    
+        
+                
+        #Tamanho da amostra
+        tamanhoDaAmostra = f"{sum(FequenciaIndividualAbsoluta.values()):g}"
+        
+        #Frequência acumulada absoluta
+        Fac = 0 
+        FacAnt = 0
+        FrequenciaAcumulada = {}
+        Posicoes = {}
+        for dado in sorted(FequenciaIndividualAbsoluta.keys()):
+            Fac = FacAnt + FequenciaIndividualAbsoluta[dado]
+            FacAjust = f"{Fac:g}"
+            FrequenciaAcumulada[dado] = FacAjust
+            
+            #Calculo de posições 
+            inicio = FacAnt + 1
+            fim = Fac
+            
+            inicio = f"{inicio:g}"
+            fim = f"{fim:g}" 
+            if(inicio == fim):
+                Posicoes[dado] = f"{fim}º"
+            else:
+                Posicoes[dado] = f"{inicio}º à {fim}º" 
+            
+            FacAnt = Fac       
+        xifi = {}
+        
+        #Média
+        for amostra, frequencia in sorted(FequenciaIndividualAbsoluta.items()):
+            xifi[amostra] = amostra * frequencia
+            
+        media = round(sum(xifi.values())/sum(FequenciaIndividualAbsoluta.values()), 2)
+
+        #Moda
+        max_freq = max(FequenciaIndividualAbsoluta.values())
+        moda = [valor for valor, freq in FequenciaIndividualAbsoluta.items() if freq == max_freq]
+        if len(moda) == len(FequenciaIndividualAbsoluta):
+            tipo_moda = 'Amodal'
+        elif len(moda) == 1:
+            tipo_moda = 'Unimodal'
+        elif len(moda) == 2:
+            tipo_moda = 'Bimodal'
+        elif len(moda) == 3:
+            tipo_moda = 'Trimodal'
         else:
-            Posicoes[dado] = f"{inicio}º à {fim}º" 
-        
-        FacAnt = Fac       
-    xifi = {}
-    
-    #Média
-    for amostra, frequencia in sorted(FequenciaIndividualAbsoluta.items()):
-        xifi[amostra] = amostra * frequencia
-        
-    media = round(sum(xifi.values())/sum(FequenciaIndividualAbsoluta.values()), 2)
+            tipo_moda = 'Multimodal'
 
-    #Moda
-    max_freq = max(FequenciaIndividualAbsoluta.values())
-    moda = [valor for valor, freq in FequenciaIndividualAbsoluta.items() if freq == max_freq]
-    if len(moda) == len(FequenciaIndividualAbsoluta):
-        tipo_moda = 'Amodal'
-    elif len(moda) == 1:
-        tipo_moda = 'Unimodal'
-    elif len(moda) == 2:
-        tipo_moda = 'Bimodal'
-    elif len(moda) == 3:
-        tipo_moda = 'Trimodal'
-    else:
-        tipo_moda = 'Multimodal'
+        #Mediana
+        def calculo_mediana(dadosDesordenados, FequenciaIndividualAbsoluta):
+            dados_mediana = []
 
-    #Mediana
-    def calculo_mediana(dadosDesordenados, FequenciaIndividualAbsoluta):
-        dados_mediana = []
-
-        if dadosDesordenados:
-            dados_mediana = sorted(dadosDesordenados)
-        elif FequenciaIndividualAbsoluta:
-            for valor, freq in FequenciaIndividualAbsoluta.items():
-                dados_mediana.extend([valor] * int(freq))
-            dados_mediana.sort()
-        else:
-            return None, [] 
+            if dadosDesordenados:
+                dados_mediana = sorted(dadosDesordenados)
+            elif FequenciaIndividualAbsoluta:
+                for valor, freq in FequenciaIndividualAbsoluta.items():
+                    dados_mediana.extend([valor] * int(freq))
+                dados_mediana.sort()
+            else:
+                return None, [] 
+            
+            n = len(dados_mediana)
+            if n % 2 == 1:
+                mediana = dados_mediana[n // 2]
+            else:
+                mediana = (dados_mediana[n // 2-1] + dados_mediana[n // 2]) / 2
+            
+            return mediana, dados_mediana
         
-        n = len(dados_mediana)
-        if n % 2 == 1:
-            mediana = dados_mediana[n // 2]
-        else:
-            mediana = (dados_mediana[n // 2-1] + dados_mediana[n // 2]) / 2
-        
-        return mediana, dados_mediana
-    
-    mediana_calculada, dados_para_template = calculo_mediana(dadosDesordenados, FequenciaIndividualAbsoluta)
-    #provavelmente vai ter que criar algum tipo de salvamento pras listas originais, funções como .sort e .extend modificam as listas originais
-    #a váriavel de frequência individual absoluta está escrita como 'Fequencia'
-          
-    return render_template("index.html", media=media, moda=moda, tipo_moda = tipo_moda, mediana=mediana_calculada,  dadosDesordenados=dadosDesordenados, FequenciaIndividualAbsoluta=FequenciaIndividualAbsoluta, tamanhoDaAmostra=tamanhoDaAmostra, FrequenciaAcumulada=FrequenciaAcumulada, Posicoes=Posicoes, FequenciaIndividualAbsolutaRecebida = {}, mostrar_modal = modal_aberto, tipo = tipo)
-
+        mediana_calculada, dados_para_template = calculo_mediana(dadosDesordenados, FequenciaIndividualAbsoluta)
+        #provavelmente vai ter que criar algum tipo de salvamento pras listas originais, funções como .sort e .extend modificam as listas originais
+        #a váriavel de frequência individual absoluta está escrita como 'Fequencia'
+            
+        return render_template("index.html", media=media, moda=moda, tipo_moda=tipo_moda, mediana=mediana_calculada, 
+        escolhaCalculo=escolhaCalculo, dadosDesordenados=dadosDesordenados, FequenciaIndividualAbsoluta=FequenciaIndividualAbsoluta,
+        tamanhoDaAmostra=tamanhoDaAmostra, FrequenciaAcumulada=FrequenciaAcumulada, Posicoes=Posicoes, 
+        FequenciaIndividualAbsolutaRecebida={}, mostrar_modal=modal_aberto, tipo=tipo, escolhaCalculoJson=escolhaCalculoJson)
+    except ValueError:
+        return render_template("index.html", erro="Você precisa inserir pelo menos um dado!", FequenciaIndividualAbsoluta={},FrequenciaAcumulada={}, 
+        Posicoes={}, FequenciaIndividualAbsolutaRecebida = {}, escolhaCalculo=[])
     
 
 if __name__ == '__main__':
