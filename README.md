89b530ef3b052eaef6fb5412abfc1ce617e147de
Isadora Bastos <isabas2803@gmail.com>
2025-09-08 12:37:26 +0000

    Correção de bugs

--- README.md
+++ README.md
@@ -5,5 +5,19 @@ Calculadora para as aulas de estatistica
 Consertar -
 
 - Botão todos no /calculo_dos_dados
-- Botão de fechar modal tem q ser X e no canto direito
-- Descer botões quando abrir o modal
+- ~~Botão de fechar modal tem q ser X e no canto direito~~
+- ~~Descer botões quando abrir o modal~~
+- Descer o input text outro
+- ~~Arrumar botão de calcular e limpar~~
+- Colocar R$
+- ~~Fazer o botão calcular chacoalhar quando a pessoa não inserir um dado~~
+- Trocar ponto por vírgula
+- Dividir a parte de inserção de dados em 2 (parte de inserir e mostrar os dados inseridos)
+- Arrumar a parte dos botões no /calcular
+- Fazer uma parte circulando os dados desordenados e a tabela, separando da outra forma de inserção de dados
+- Deixar todos os botões das medidas e cálculos do mesmo tamanho 
+- Arrumar o fundo
+- Divisória entre a parte de inserção dos dados e dos cálculos para ficar bonito 
+- Na parte de inserção de dados, quando a pessoa inserir um dado, volta automaticamente para a parte de input
+- Obrigar a pessoa a escrever algo no outro
+- Esconder a parte de Estatísticas desses Dados para baixo
--- static/script.js
+++ static/script.js
@@ -1,27 +1,30 @@
-const open = document.getElementById('open');
 const container_modal_desordenado = document.getElementById('container_modal_desordenado');
 const container_modal_tabela = document.getElementById('container_modal_tabela');
-const close = document.getElementById('close');
 const botoes = document.querySelector('.botoes-calcular-limpar');
+const tipo_dado = document.querySelector(".container-opcoes-tipo-dado");
 
 
 function abrirModalDesordenado() {
     container_modal_desordenado.classList.add('show');
     botoes.classList.add('descer');
+    tipo_dado.classList.add("descer");
 }
     
 function fecharModalDesordenado() {
     container_modal_desordenado.classList.remove('show');
     botoes.classList.remove('descer');
+    tipo_dado.classList.remove("descer");
 }
 
 function abrirModalTabela() {
     container_modal_tabela.classList.add('show');
     botoes.classList.add('descer');
+    tipo_dado.classList.add("descer");
 }
     
 function fecharModalTabela() {
     container_modal_tabela.classList.remove('show');
     botoes.classList.remove('descer');
+    tipo_dado.classList.remove("descer");
 }
    
